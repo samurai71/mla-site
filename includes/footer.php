d93843a9a72f1de0d3bb61a3c7ae296d31c50962
Mark Landeryou <marklanderyou@hotmail.com>
2018-01-09 11:24:17 -0500

    updated

--- includes/footer.php
+++ includes/footer.php
@@ -5,22 +5,18 @@
                     <img src="img/cdn.gif" alt="Canadian Flag">
                   </div>
                   <div class="small-12 medium-4  large-4 cell  float-right">
-                  <!-- <h4>London's Weather</h4> -->
-                  <!-- <div id="plemx-root">
-                  <a href="http://www.theweathernetwork.com">The Weather Network</a></div> 
-                  </div> -->
-                  <!-- <div id="plemx-root"></div> 
-                    <a href="http://www.theweathernetwork.com">The Weather Network</a> -->
                   <div class="small-12 medium-12  large-12 cell">
                     <p id="copyright">© 2015 - 2018. All Rights reserved. <a href="privacypolicy.html" target="_blank">Privacy Policy</a></p>
                 </div>
                 </div>
               </div>
-    </footer>   
-    <script src="js/app.js"></script>
-    <script src="js/icons.js"></script>
-    <script src="js/weather.js"></script>
+    </footer>  
+    <script src="js/app.min.js"></script>
+    <script src="js/icons.min.js"></script>
     <script src="js/google.js"></script>
     <script src="https://cdn.plyr.io/2.0.18/plyr.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.slicknav.min.js"></script>
+    <script src="js/script.js"></script>
   </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -5,16 +5,19 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo($pageTitle); ?> | Mark Landeryou Artisan, London Ontario Canada</title>
-    <link rel="stylesheet" href="css/app.css">
-    <link rel="stylesheet" href="css/icons.css">
+    <link rel="stylesheet" href="css/app.min.css">
+    <link rel="stylesheet" href="css/icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css">
     <link rel="stylesheet" href="https://cdn.plyr.io/2.0.18/plyr.css">
+    <link rel="stylesheet" href="css/slicknav.min.css">
     <script src="https://use.fontawesome.com/264e791b25.js"></script>
+    <script src="js/modernizr-custom.min.js"></script>
     <link rel="icon" href="img/favicon.ico" type="image/x-icon"/>
   </head>
   <body>
     
     <header>
+    <h1 class="hidden">Logo and Navigation</h1>
       <div class="title-bar" data-responsive-toggle="responsive-menu" data-hide-for="medium">
         <button class="menu-icon" type="button" data-toggle="responsive-menu"></button>
         <div class="title-bar-title">Menu</div>
@@ -22,7 +25,7 @@
     
       <div class="top-bar" id="responsive-menu">
         <div class="top-bar-left">
-        <a href="index.php"><img src="img/MLA-Logo-Outlined.svg" alt="Mark Landeryou Aritsan Logo"  class="logo"></a>
+        <a href="index.php"><img src="img/MLA-Logo-Outlined.svg" alt="Mark Landeryou Artisan Logo"  class="logo"></a>
         </div>
         <div class="top-bar-right">
           <ul class="dropdown menu" data-dropdown-menu>
@@ -33,4 +36,17 @@
           </ul>
         </div>
       </div>
+      <div class="hide-for-large">
+      <img src="img/MLA-Logo-Outlined.svg" alt="Mark Landeryou Artisan Logo">
+      <nav class="hide-for-medium-up" id="mobileNav">
+            <h2 class="hidden">Mobile Navigation Menu</h2>
+              <!-- <img src="img/MLA-Logo-Outlined.svg" alt="Mark Landeryou Artisan Logo"> -->
+              <ul id="menu">
+              <li><a href="index.php">About</a></li>
+              <li><a href="portfolio.php">Portfolio</a></li>
+              <li><a href="services.php">Services</a></li>
+              <li><a href="contact.php">Contact</a></li>
+            </ul>
+          </nav>
+          </div>
     </header>   
