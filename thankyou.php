efccd4ec9e94f275bf0d1976dc415a2d83d704e0
samurai71 <samurai96@hotmail.com>
2018-01-09 13:03:21 -0500

    edited thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -1,7 +1,7 @@
 <?php 
 include 'includes/header.php';
 ?>
-<section>
+<section class="content-wrap">
 <h2>Thank you</h2>
 <p>Thank you for filling out the contact form. We will get back to you as soon as we can.</p>
 </section>
