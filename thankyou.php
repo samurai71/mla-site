dd7d1f30ba171f64405aca8c76c92b46349980f6
samurai71 <samurai96@hotmail.com>
2018-01-09 12:56:10 -0500

    uploaded thankyou.php

---
+++ thankyou.php
@@ -0,0 +1,12 @@
+<?php 
+include 'includes/header.php';
+?>
+<section>
+<h2>Thank you</h2>
+<p>Thank you for filling out the contact form. We will get back to you as soon as we can.</p>
+</section>
+
+<?php
+include 'includes/socialmedia.php';
+include 'includes/footer.php';
+?>
